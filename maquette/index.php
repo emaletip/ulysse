4505d3e8458e81d2139f41f83fdb02f2bfbc2a86
Leiline <pauline.esse10@gmail.com>
2015-06-02 10:12:59 +0200

    Mis en place du grid bootstrap pour faciliter le placement de bloc.

--- maquette/index.php
+++ maquette/index.php
@@ -3,8 +3,44 @@
 	<head>
 		<meta charset="utf-8">
 		<title>La maquette du site</title>
-		<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
-		<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
+		<link rel="stylesheet" type="text/css" href="../vendor/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../vendor/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../vendor/css/styles.css">
 	</head>
-	<body></body>
+	<body>
+		<!-- HEADER -->
+		<header class="clearfix">
+			<div class="col-md-4 left">
+				<div id="block-login" class="block">Connexion ou inscription</div>
+			</div>
+			<div class="col-md-4 middle">
+			</div>
+			<div class="col-md-4 right">
+				<div id="block-panier" class="block">Panier</div>
+			</div>
+		</header>
+		<!-- END HEADER -->
+
+		<!-- CONTENT -->
+		<div id="content-wrapper-parent">
+			<div class="col-md-4 left">
+			</div>
+			<div class="col-md-4 middle">
+			</div>
+			<div class="col-md-4 right">
+			</div>
+		</div>
+		<!-- END CONTENT -->
+
+		<!-- FOOTER -->
+		<footer>
+			<div class="col-md-4 left">
+			</div>
+			<div class="col-md-4 middle">
+			</div>
+			<div class="col-md-4 right">
+			</div>
+		</footer>
+		<!-- END FOOTER -->
+	</body>
 </html>
