3ce0f0410feaf1f6979a64a0f76ab8acd08a6d20
Leiline <pauline.esse10@gmail.com>
2015-06-02 09:47:57 +0200

    Création de la page de maquette du site.

---
+++ maquette/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>La maquette du site</title>
+		<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+		<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
+	</head>
+	<body></body>
+</html>
