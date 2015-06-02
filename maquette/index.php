ce0d82052f8e8bb6419768193210ddec0b6b3c69
Leiline <pauline.esse10@gmail.com>
2015-06-02 15:28:10 +0200

    La mise en bloc des chatons comme c'est zolie.

--- maquette/index.php
+++ maquette/index.php
@@ -20,15 +20,36 @@
 					<div class="col-md-4 middle">
 					</div>
 					<div class="col-md-4 right">
-						<div id="block-search" class="block"><input type="text" placeholder="Recherche" /></div>
-						<div id="block-cart" class="block">Panier</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div id="block-search" class="block"><input type="text" placeholder="Recherche" /></div>
+							</div>
+							<div class="col-md-6">
+								<div id="block-cart" class="block">Panier</div>
+							</div>
+						</div>
 					</div>
 				</div>
 
 				<!-- Navigation -->		
 				<div class="row">
 					<div class="col-md-12">
-						<nav>Le menu</nav>
+						<nav>
+							<ul id="navigation" class="nav navbar-nav">
+								<li>
+									<a href="#">Des liens !</a>
+								</li>
+								<li>
+									<a href="#">Des liens !</a>
+								</li>
+								<li>
+									<a href="#">Des liens !</a>
+								</li>
+								<li>
+									<a href="#">Des liens !</a>
+								</li>
+							</ul>
+						</nav>
 					</div>
 				</div>
 			</div>
@@ -36,7 +57,7 @@
 			<div class="container-fluid">
 				<!-- Slideshow -->
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-md-12 slideshow">
 						<div id="block-slideshow" class="block">
 							<img class="img-responsive" src="img/cats-q-c-1920-1250-5.jpg" alt="UN CHAT !" />
 						</div>
@@ -48,7 +69,38 @@
 		<!-- END HEADER -->
 
 		<!-- CONTENT -->
-		<div id="content-wrapper-parent">
+		<div id="content-wrapper">
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-md-4">
+						<div class="block" id="block-produit-phare">
+							<a href="#">
+								<img class="img-responsive" src="img/cats-q-c-600-600-1.jpg" alt="DES CHATS !">
+							</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="block" id="block-produit-phare">
+							<a href="#">
+								<img class="img-responsive" src="img/cats-q-c-600-600-4.jpg" alt="DES CHATS !">
+							</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="block" id="block-produit-phare">
+							<a href="#">
+								<img class="img-responsive" src="img/cats-q-c-600-600-9.jpg" alt="DES CHATS !">
+							</a>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="row"></div>
+				<div class="row"></div>
+			</div>
 		</div>
 		<!-- END CONTENT -->
 
