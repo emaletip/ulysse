c094fe4e2df2657b725ab1ce28c157957d9661c1
Leiline <pauline.esse10@gmail.com>
2015-06-02 16:50:38 +0200

    Le commit final

--- maquette/index.php
+++ maquette/index.php
@@ -6,6 +6,9 @@
 		<link rel="stylesheet" type="text/css" href="../vendor/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="../vendor/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="../vendor/css/styles.css">
+		<script type="text/javascript" src="../vendor/js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="../vendor/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="../vendor/js/npm.js"></script>
 	</head>
 	<body>
 		<!-- HEADER -->
@@ -15,7 +18,7 @@
 				<!-- Connection, inscription and cart -->
 				<div class="row">
 					<div class="col-md-4 left">
-						<div id="block-login" class="block">Connexion ou inscription</div>
+						<div id="block-login" class="block"><a href="#">Connexion</a> ou <a href="#">Inscription</a></div>
 					</div>
 					<div class="col-md-4 middle">
 					</div>
@@ -34,20 +37,45 @@
 				<!-- Navigation -->		
 				<div class="row">
 					<div class="col-md-12">
-						<nav>
-							<ul id="navigation" class="nav navbar-nav">
-								<li>
-									<a href="#">Des liens !</a>
-								</li>
-								<li>
-									<a href="#">Des liens !</a>
-								</li>
-								<li>
-									<a href="#">Des liens !</a>
-								</li>
-								<li>
-									<a href="#">Des liens !</a>
-								</li>
+
+						<nav class="navbar navbar-default">
+							<ul id="navigation" class="nav navbar-nav">								
+								<li class="first-ul"><a class="list-title" href="#">Home</a></li>
+								<li class="first-ul dropdown">
+						          <a href="#" class="list-title dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shop<span class="caret"></span></a>
+						          <ul class="dropdown-menu" role="menu">
+						            <li>
+						            	<ul>
+						            		<li class="list-title">Catégorie 1</li>
+						            		<li><a href="#">Produit 1</a></li>
+						            		<li><a href="#">Produit 2</a></li>
+						            		<li><a href="#">Produit 3</a></li>
+						            		<li><a href="#">Produit 4</a></li>
+						            	</ul>
+						            </li>
+						            <li>
+						            	<ul>
+						            		<li class="list-title">Catégorie 2</li>
+						            		<li><a href="#">Produit 1</a></li>
+						            		<li><a href="#">Produit 2</a></li>
+						            		<li><a href="#">Produit 3</a></li>
+						            		<li><a href="#">Produit 4</a></li>
+						            	</ul>
+						            </li>
+						            <li>
+						            	<ul>
+						            		<li class="list-title">Catégorie 3</li>
+						            		<li><a href="#">Produit 1</a></li>
+						            		<li><a href="#">Produit 2</a></li>
+						            		<li><a href="#">Produit 3</a></li>
+						            		<li><a href="#">Produit 4</a></li>
+						            	</ul>
+						            </li>
+						          </ul>
+						        </li>
+						        <li class="first-ul"><a class="logo">Le logo lol</a></li>
+								<li class="first-ul"><a class="list-title" href="#">About</a></li>
+								<li class="first-ul"><a class="list-title" href="#">Contact</a></li>
 							</ul>
 						</nav>
 					</div>
@@ -71,35 +99,23 @@
 		<!-- CONTENT -->
 		<div id="content-wrapper">
 			<div class="container">
-
 				<div class="row">
-
 					<div class="col-md-4">
-						<div class="block" id="block-produit-phare">
-							<a href="#">
-								<img class="img-responsive" src="img/cats-q-c-600-600-1.jpg" alt="DES CHATS !">
-							</a>
-						</div>
+						<a href="#">
+							<img class="img-responsive" src="img/cats-q-c-600-600-1.jpg" alt="DES CHATS !">
+						</a>
 					</div>
 					<div class="col-md-4">
-						<div class="block" id="block-produit-phare">
-							<a href="#">
-								<img class="img-responsive" src="img/cats-q-c-600-600-4.jpg" alt="DES CHATS !">
-							</a>
-						</div>
+						<a href="#">
+							<img class="img-responsive" src="img/cats-q-c-600-600-4.jpg" alt="DES CHATS !">
+						</a>
 					</div>
 					<div class="col-md-4">
-						<div class="block" id="block-produit-phare">
-							<a href="#">
-								<img class="img-responsive" src="img/cats-q-c-600-600-9.jpg" alt="DES CHATS !">
-							</a>
-						</div>
+						<a href="#">
+							<img class="img-responsive" src="img/cats-q-c-600-600-9.jpg" alt="DES CHATS !">
+						</a>
 					</div>
-
 				</div>
-
-				<div class="row"></div>
-				<div class="row"></div>
 			</div>
 		</div>
 		<!-- END CONTENT -->
