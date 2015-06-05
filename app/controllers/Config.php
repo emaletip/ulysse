37c61af23a065047e4271b47dccc5bdcf6dffd71
emaletip <emalet.ip@gmail.com>
2015-06-05 15:47:12 +0200

    img

--- app/controllers/Config.php
+++ app/controllers/Config.php
@@ -16,13 +16,26 @@ class Config {
 	}
 
 	public function postConfig_update() {
-		
-		$file = handleFile($_FILES['logo'], 'Config');
-		//var_dump($file);
-		die;
+		$dirimg = 'Config';
+				
+		if(isset($_FILES) && $_FILES['logo']['name'] != '') {
+			if(isset($_POST['old_img'])) {
+				$old_img = __DIR__.'/../../'.$_POST['old_img'];
+				if(file_exists($old_img)) {
+					unlink($old_img);
+				}
+				unset($_POST['old_img']);
+			}
+			$_POST['logo'] = handleFile($_FILES['logo'], $dirimg);
+		} else {
+			if(isset($_POST['old_img'])) {
+				unset($_POST['old_img']);
+			}
+			unset($_POST['logo']);
+		}
 	
 		$this->configModel->updateConfig($_POST);
-		
+
 		$_SESSION['flash']['config']['key'] = 'success';
 		$_SESSION['flash']['config']['msg'] = '<b>Félicitations ! </b> Vos données ont bien été enregistrées.';
 		$_SESSION['flash']['config']['time'] = time() + 2;
