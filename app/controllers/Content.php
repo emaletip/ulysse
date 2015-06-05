cde5ab5c06f2123f7f014f1bad74ff41eb6108b0
Yoshun <tonytetrel@gmail.com>
2015-06-05 14:29:41 +0200

    content controller + model

--- app/controllers/Content.php
+++ app/controllers/Content.php
@@ -3,5 +3,19 @@
 namespace app\controllers;
 
 class Content {
-	 
+    
+	 private $contentModel;
+
+	public function __construct() {
+		$this->contentModel = new \app\models\Content();
+		return $this;
+	}
+
+	public function getContent($id) {
+		//return $this->contentModel->getConfig();
+	}
+
+	public function getList() {
+        return $this->contentModel->getList();
+    }
 }	
--- app/models/Content.php
+++ app/models/Content.php
@@ -4,10 +4,29 @@ namespace app\models;
 
 class Content {
 
+    private $pdo;
 	private $id;
 	private $created_date;
 	private $content_type_name;
 	private $created_user;
+    
+    public function getList(){
+        $query = 'SELECT c.*, ft.*, fp.*, c.id AS content_id, t.*, t.id AS content_type_id FROM `content` c
+        JOIN `content_type` t ON c.content_type_name = t.name
+        JOIN `field_title` ft ON c.id = ft.content_id
+        JOIN `field_price` fp ON c.id = fp.content_id
+        WHERE c.`content_type_name` = \'product\'';
+        $results = $this->pdo->query($query);
+        return $results;
+    }
+    
+    public function __construct() {
+		$this->pdo = new \config\database();
+	}
+	
+	public function getPdo() {
+		return $this->pdo;	
+	}
 	
 	public function getId() {
 		return $this->id;
@@ -33,15 +52,6 @@ class Content {
 	
 	public function setContent_type_name($content_type_name) {
 		$this->content_type_name = $content_type_name;
-		return $this;
-	}
-	
-	public function getCreated_date() {
-		return $this->created_date;
-	}
-	
-	public function setCreated_date($created_date) {
-		$this->created_date = $created_date;
 		return $this;
 	}
 	 
